f2e7cba07d48d613cb5a5b24e562276649078d3a
lvhaizhou <417703682@qq.com>
2022-07-28 14:57:35 +0800

    feat(drop-down): 右键下拉菜单

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,93 @@
+import { computed, createVNode, defineComponent, inject, onBeforeUnmount, onMounted, provide, reactive, ref, render } from "vue";
+
+export const DropdownItem = defineComponent({
+  props: {
+    label: String,
+    icon: String,
+  },
+  setup(props) {
+    const hide = inject("hide");
+
+    return () => <div class="dropdown-item" onClick={hide}>
+      <i class={props.icon}></i>
+      <span>{props.label}</span>
+    </div>;
+  },
+});
+
+const DialogComponent = defineComponent({
+  props: {
+    option: { type: Object },
+  },
+  setup(props, ctx) {
+    const state = reactive({
+      option: props.option,
+      isShow: false,
+      top: 0,
+      left: 0,
+    });
+    ctx.expose({
+      showDropDown(option) {
+        state.option = option;
+        state.isShow = true;
+        const { top, left, height } = option.el.getBoundingClientRect();
+        state.top = top + height;
+        state.left = left;
+      },
+    });
+
+    provide("hide", () => (state.isShow = false));
+
+    const classes = computed(() => [
+      "dropdown",
+      {
+        "dropdown-isShow": state.isShow,
+      },
+    ]);
+    const styles = computed(() => ({
+      top: state.top + "px",
+      left: state.left + "px",
+    }));
+
+    const el = ref(null);
+    const onMousedownDocument = (e: MouseEvent) => {
+      if (!el.value.contains(e.target)) { // 如果点击的是dropdown内部，则什么都不做
+        state.isShow = false;
+      }
+    };
+
+    onMounted(() => {
+      // 事件的传递行为，是先捕获再冒泡的
+      // 之前为了阻止事件传播，我们给block都增加了 stopPropagation
+      document.addEventListener("mousedown", onMousedownDocument, true);
+    });
+    onBeforeUnmount(() => {
+      document.removeEventListener("mousedown", onMousedownDocument);
+    });
+    return () => {
+      return <div class={classes.value} style={styles.value} ref={el}>
+        {state.option.content()}
+      </div>;
+    };
+  },
+});
+
+let vNode;
+
+export function $dropdown(option) {
+  // element-plus中有el-dialog组件
+  // 手动挂载组件
+
+  if (!vNode) {
+    const el = document.createElement("div");
+    // 将组件渲染成虚拟节点
+    vNode = createVNode(DialogComponent, { option });
+
+    // render：把虚拟节点变成真实节点
+    // 将组件渲染到这个el元素上
+    render(vNode, el);
+    document.body.appendChild(el);
+  }
+  const { showDropDown } = vNode.component.exposed;
+  showDropDown(option);
+}
--- src/packages/editor.tsx
+++ src/packages/editor.tsx
@@ -1,6 +1,8 @@
 import deepcopy from "deepcopy";
+import { ElButton } from "element-plus";
 import { computed, defineComponent, inject, ref } from "vue";
 import { $dialog } from "~/components/Dialog";
+import { $dropdown, DropdownItem } from "~/components/Dropdown";
 import { useBlockDragger } from "~/hooks/useBlockDragger";
 import { useCommand } from "~/hooks/useCommand";
 import { useFocus } from "~/hooks/useFocus";
@@ -16,6 +18,7 @@ export default defineComponent({
   setup(props, { emit }) {
     // 预览的时候，内容不能再操作了，可以点击输入内容，方便看效果
     const previewRef = ref(false);
+    const editorRef = ref(true);
 
     const config: any = inject("config");
     const data = computed({
@@ -84,66 +87,108 @@ export default defineComponent({
           clearBlockFocus();
         },
       },
+      {
+        label: "关闭",
+        icon: "icon-close",
+        handler: () => {
+          editorRef.value = false;
+          clearBlockFocus();
+        },
+      },
     ];
 
-    return () => <div class="editor">
-      <div class="editor-left">
-        {/* 根据注册列表，渲染对应的内容 */}
-        {config.componentList.map(component => (
-          <div
-            class="editor-left-item"
-            draggable
-            onDragstart={e => dragstart(e, component)}
-            onDragend={dragend}
-          >
-            <span>{component.label}</span>
-            <div>{component.preview()}</div>
-          </div>
-        ))}
-      </div>
-      <div class="editor-top">
-        {buttons.map((btn, index) => {
-          const icon = typeof btn.icon === "function" ? btn.icon() : btn.icon;
-          const label = typeof btn.label === "function" ? btn.label() : btn.label;
-          return <div class="editor-top-button" onClick={btn.handler}>
-            <i class={icon}></i>
-            <span>{label}</span>
-          </div>;
-        })}
-      </div>
-      <div class="editor-right">属性控制栏</div>
-      <div class="editor-container">
-        {/* 负责产生滚动条（页面比较长的情况下） */}
-        <div class="editor-container-canvas">
-          {/* 内容区域 */}
-          <div
-            ref={containerRef}
-            class="editor-container-canvas__content"
-            style={containerStyles.value}
-            onMousedown={containerMousedown}
-          >
-            {
-              data.value.blocks.map((block, index) => (
-                <EditorBlock
-                  key={block}
-                  class={[
-                    block.focus ? "editor-block-focus" : "",
-                    previewRef.value ? "editor-block-preview" : "",
-                  ]}
-                  block={block}
-                  {...{
-                    onMousedown: (e: MouseEvent) => blockMousedown(e, block, index),
-                  }}
-                ></EditorBlock>
-              ))
-            }
+    const onContextMenuBlock = (e, block) => {
+      e.preventDefault();
 
-            { markLine.x !== null && <div class="line-x" style={{ left: markLine.x + "px" }}></div> }
-            { markLine.y !== null && <div class="line-y" style={{ top: markLine.y + "px" }}></div> }
+      $dropdown({
+        el: e.target, // 以哪个元素为准，产生一个dropdown
+        content: () => <>
+          <DropdownItem label="删除" icon="icon-delete" {...{ onClick: () => commands.delete() }}></DropdownItem>
+          <DropdownItem label="置顶" icon="icon-place-top" {...{ onClick: () => commands.placeTop() }}></DropdownItem>
+          <DropdownItem label="置底" icon="icon-place-bottom" {...{ onClick: () => commands.placeBottom() }}></DropdownItem>
+          <DropdownItem label="查看" icon="icon-browse" {...{ onClick: () => commands.delete() }}></DropdownItem>
+          <DropdownItem label="导入" icon="icon-import" {...{ onClick: () => commands.delete() }}></DropdownItem>
+        </>,
+      });
+    };
+
+    return () => !editorRef.value
+      ? <>
+        <div
+          ref={containerRef}
+          class="editor-container-canvas__content"
+        >
+          {
+            data.value.blocks.map((block, index) => (
+              <EditorBlock
+                key={block}
+                block={block}
+                class="editor-block-preview"
+              ></EditorBlock>
+            ))
+          }
+        </div>
+        <div><ElButton type="primary" onClick={() => (editorRef.value = true)}>返回编辑</ElButton></div>
+      </>
+      : <div class="editor">
+        <div class="editor-left">
+          {/* 根据注册列表，渲染对应的内容 */}
+          {config.componentList.map(component => (
+            <div
+              class="editor-left-item"
+              draggable
+              onDragstart={e => dragstart(e, component)}
+              onDragend={dragend}
+            >
+              <span>{component.label}</span>
+              <div>{component.preview()}</div>
+            </div>
+          ))}
+        </div>
+        <div class="editor-top">
+          {buttons.map((btn, index) => {
+            const icon = typeof btn.icon === "function" ? btn.icon() : btn.icon;
+            const label = typeof btn.label === "function" ? btn.label() : btn.label;
+            return <div class="editor-top-button" onClick={btn.handler}>
+              <i class={icon}></i>
+              <span>{label}</span>
+            </div>;
+          })}
+        </div>
+        <div class="editor-right">属性控制栏</div>
+        <div class="editor-container">
+          {/* 负责产生滚动条（页面比较长的情况下） */}
+          <div class="editor-container-canvas">
+            {/* 内容区域 */}
+            <div
+              ref={containerRef}
+              class="editor-container-canvas__content"
+              style={containerStyles.value}
+              onMousedown={containerMousedown}
+            >
+              {
+                data.value.blocks.map((block, index) => (
+                  <EditorBlock
+                    key={block}
+                    class={[
+                      block.focus ? "editor-block-focus" : "",
+                      previewRef.value ? "editor-block-preview" : "",
+                    ]}
+                    block={block}
+                    {...{
+                      onMousedown: (e: MouseEvent) => blockMousedown(e, block, index),
+                      onContextmenu: (e) => onContextMenuBlock(e, block),
+                    }}
+                  ></EditorBlock>
+                ))
+              }
+
+              { markLine.x !== null && <div class="line-x" style={{ left: markLine.x + "px" }}></div> }
+              { markLine.y !== null && <div class="line-y" style={{ top: markLine.y + "px" }}></div> }
+            </div>
           </div>
         </div>
-      </div>
 
-    </div>;
+      </div>;
   },
 });
